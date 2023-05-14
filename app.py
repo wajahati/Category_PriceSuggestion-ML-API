45ff3c1c66b78f08dd064d2e14a3b134f77e516f
Wajahat Ahmad <67877858+wajahati@users.noreply.github.com>
2023-05-14 09:28:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ import re
 from keras.models import load_model
 import operator
 
-import openai
-import re
+# import openai
+# import re
 
 # 2. Create the app object
 app = FastAPI()
@@ -414,50 +414,50 @@ def findCategory(name,description):
   return data_array
 
 
-def suggest(title,description):
+# def suggest(title,description):
     
-    openai.api_key = "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"
-    prompt = f"Based on the following product information in Pakistan in pakistani rupees, suggest a price range :\n\nTitle: {title}\nDescription: {description}\n\nSuggested Price:"
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=64,
-        n=1,
-        stop=None,
-        temperature=0.5,
-        )
+#     openai.api_key = "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"
+#     prompt = f"Based on the following product information in Pakistan in pakistani rupees, suggest a price range :\n\nTitle: {title}\nDescription: {description}\n\nSuggested Price:"
+#     response = openai.Completion.create(
+#         engine="text-davinci-002",
+#         prompt=prompt,
+#         max_tokens=64,
+#         n=1,
+#         stop=None,
+#         temperature=0.5,
+#         )
 
-    response_text = response.choices[0].text.strip()
-    print(response_text)
-    # Define a regular expression pattern to match the price range
-    price_pattern = r"^(PKR|(Rs\.) )?\d{1,3}(,\d{3})*(\.\d+)?\s*-\s*(Rs\. )?\d{1,3}(,\d{3})*(\.\d+)?$"
+#     response_text = response.choices[0].text.strip()
+#     print(response_text)
+#     # Define a regular expression pattern to match the price range
+#     price_pattern = r"^(PKR|(Rs\.) )?\d{1,3}(,\d{3})*(\.\d+)?\s*-\s*(Rs\. )?\d{1,3}(,\d{3})*(\.\d+)?$"
     
-    # Search the response text for the price pattern
-    match = re.search(price_pattern, response_text)
+#     # Search the response text for the price pattern
+#     match = re.search(price_pattern, response_text)
     
-    # If a match is found, extract the price range
-    if match:
-        price_range = match.group()
-        return price_range
-    else:
-        return("Could not find price range in response")
-    
-
-# 3. Index route, opens automatically on http://127.0.0.1:8000
-@app.get('/')
-def index():
-    return {'message': 'Hello, World'}
+#     # If a match is found, extract the price range
+#     if match:
+#         price_range = match.group()
+#         return price_range
+#     else:
+#         return("Could not find price range in response")
     
 
-@app.post('/suggestPrice')
-def suggestPrice(data:Price):
-    data = data.dict()
-    title=data['title']
-    description=data['description']
+# # 3. Index route, opens automatically on http://127.0.0.1:8000
+# @app.get('/')
+# def index():
+#     return {'message': 'Hello, World'}
     
-    suggested = suggest(title,description)
-    data = {"suggestedprice": suggested}
-    return JSONResponse(content=data)
+
+# @app.post('/suggestPrice')
+# def suggestPrice(data:Price):
+#     data = data.dict()
+#     title=data['title']
+#     description=data['description']
+    
+#     suggested = suggest(title,description)
+#     data = {"suggestedprice": suggested}
+#     return JSONResponse(content=data)
 
 @app.post('/predictCategory')
 def predictCategory(data:Category):
